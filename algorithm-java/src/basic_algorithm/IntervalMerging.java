1c04dea8b93f6237f8d7d9fe15a6619cf0555f62
Agility6 <agility1013@gmail.com>
2023-11-17 13:56:41 +0800

    update: discretization intervalmerging

--- algorithm-java/src/basic_algorithm/IntervalMerging.java
+++ algorithm-java/src/basic_algorithm/IntervalMerging.java
@@ -2,77 +2,80 @@ package basic_algorithm;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
 
 public class intervalMerging {
     public static void main(String[] args) {
         
+        /**
+         * 给定 n 个区间 [li,ri] ，要求合并所有有交集的区间。
+         * 注意如果在端点处相交，也算有交集。输出合并完成后的区间个数。
+         * 例如：[1,3] 和 [2,6] 可以合并为一个区间 [1,6]。
+         * 
+         * 数据范围
+         * 1 ≤ n ≤ 100000,
+         * −10^9 ≤ li ≤ ri ≤ 10^9
+         * 
+         * 输入样例
+         * 5
+         * 1 2
+         * 2 4
+         * 5 6
+         * 7 8
+         * 7 9
+         * 
+         * 输出样例
+         * 3
+         */
+
+         Scanner sc = new Scanner(System.in);
+
+         int n = sc.nextInt();
+
+         List<int[]> nums = new ArrayList<>();
+
+         while (n-- > 0) {
+
+            int l = sc.nextInt();
+            int r = sc.nextInt();
+
+            nums.add(new int[] {l, r});
+         }
+
+         System.out.println(merge(nums));
     }
 
-    /**
-     * 将所有存在交集的区间合并
-     * 输出区间合并之后的总数
-     * int[0]存左端点 int[1]存右端点 
-     * @param list
-     */
-    public static void merge(ArrayList<int []> list) {
+    public static int merge(List<int[]> nums) {
 
-        // 1. 对列表中每个数组0位置元素升序排序
-        list.sort(new Comparator<int []>() {
+        nums.sort(new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
                 return o1[0] - o2[0];
             }
         });
 
-        int res = 0;
-        int r = Integer.MIN_VALUE;
-        for (int a[] : list) {
-            
-            // 下一个区间的左端点大于当前区间的右端点，res++
-            if (a[0] > r) res++;
-            else r = Math.max(r, a[1]); // 否则更新右端点
-        }
-
-        System.out.println(res);
-    }
-
-    /**
-     * 将每个合并区间添加到一个新列表中
-     * @param list
-     * @return
-     */
-    public static int merge2(ArrayList<int[]> list) {
-        
-        ArrayList<int []> res = new ArrayList<>();
-
-        list.sort(new Comparator<int []>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] - o2[0]; 
-            }
-        });
-
         int l = Integer.MIN_VALUE;
         int r = Integer.MIN_VALUE;
 
-        for (int[] a : list) {
-            if (a[0] > r) {
-                // 特判初始化情况
-                if (l != Integer.MIN_VALUE) {
-                    res.add(new int[] {l, r});
-                }
-                l = a[0];
-                r = a[1];
+        List<int[]> res = new ArrayList<>();
+
+        for (int[] arr : nums) {
+
+            if (arr[0] > r) {
+
+                if (l != Integer.MIN_VALUE) res.add(new int[] {l, r});
+                l = arr[0];
+                r = arr[1];
+
             } else {
-                r = Math.max(a[1], r);
+                r = Math.max(r, arr[1]);
             }
         }
 
-        // 手动将最后一个区间添加到res中
-        if (l != Integer.MIN_VALUE) {
-            res.add(new int[] {l, r});
-        }
+        if (l != Integer.MIN_VALUE) res.add(new int[] {r, l});
 
         return res.size();
     }
+
 }
--- algorithm-java/src/basic_algorithm/discretization.java
+++ algorithm-java/src/basic_algorithm/discretization.java
@@ -1,58 +1,136 @@
 package basic_algorithm;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class discretization {
+
+    static int N = 300010; // 因为有n个和2m个数字 一共300000
+    static List<Integer> alls = new ArrayList<>(); // 离散化数组
+    static int[] a = new int[N]; // 记录添加c
+
     public static void main(String[] args) {
         
+        /**
+         * 假定有一个无限长的数轴，数轴上每个坐标上的数都是 0。
+         * 现在，我们首先进行 n 次操作，每次操作将某一位置 x 上的数加 c。
+         * 接下来，进行 m 次询问，每个询问包含两个整数 l 和 r ，你需要求出在区间 [l,r]之间的所有数的和。
+         * 
+         * 数据范围 −10^9 ≤ x ≤ 10^9,
+         * 1 ≤ n, m ≤ 105,
+         * −10^9 ≤ l ≤ r ≤ 10^9,
+         * −10000 ≤ c ≤ 10000
+         * 
+         * 输入样例
+         * 3 3
+         * 1 2
+         * 3 6
+         * 7 5
+         * 1 3
+         * 4 6
+         * 7 8
+         * 
+         * 输出样例
+         * 8
+         * 0
+         * 5
+         */
+
+         Scanner sc = new Scanner(System.in);
+
+         int n = sc.nextInt();
+         int m = sc.nextInt();
+
+         List<Node> add = new ArrayList<>(); // 记录n次添加
+         List<Node> query = new ArrayList<>(); // 记录m次查询
+
+         while (n-- > 0) {
+            
+            int k = sc.nextInt();
+            int c = sc.nextInt();
+
+            add.add(new Node(k, c));
+            alls.add(k);
+
+         }
+
+         while (m-- > 0) {
+            
+            int l = sc.nextInt();
+            int r = sc.nextInt();
+
+            query.add(new Node(l, r));
+            alls.add(l);
+            alls.add(r);
+
+         }
+
+         Collections.sort(alls);
+         int unique = unique();
+         alls.subList(0, unique);
+
+         for (Node node : add) {
+
+            int k = find(node.first);
+            a[k] += node.second;
+         }
+
+         for (int i = 1; i <= alls.size(); i++) {
+            a[i] += a[i - 1];
+         }
+
+         for (Node node : query) {
+
+            int l = find(node.first);
+            int r = find(node.second);
+
+            System.out.println(a[r] - a[l - 1]);
+         }
+
+
     }
 
-    /**
-     * 数组去重
-     * @param list
-     * @return 
-     */
-    public static int unique(List<Integer> list) {
+    public static int find(int t) {
+
+        int l = 0;
+        int r = alls.size() - 1;
+
+        while (l < r) {
+
+            int mid = (l + r) >> 1;
+
+            if (alls.get(mid) >= t) r = mid;
+            else l = mid + 1;
+        }
+
+        return l + 1;
+    }
+
+    public static int unique() {
 
         int j = 0;
-        for (int i = 0; i < list.size(); i++) {
-            if (i == 0 || !list.get(i).equals(list.get(i - 1))) {
-                list.set(j, list.get(i));
+        for (int i = 0; i < alls.size(); i++) {
+            
+            if (i == 0 || !alls.get(i).equals(alls.get(j - 1))) {
+                alls.set(j, alls.get(i));
                 j++;
             }
         }
 
         return j;
     }
-
-    /**
-     * 二分求出x对应的离散化的值
-     * @param x
-     * @param list
-     * @return
-     */
-    public static int find(int x, List<Integer> list) {
-
-        int l = 0;
-        int r = list.size() - 1;
-
-        while (l < r) {
-            int mid = (l + r) >> 1;
-            if (list.get(mid) >= x) r = mid;
-            else l = mid + 1;
-        }
-
-        return r + 1; // 映射1, 2
-    }
-
 }
 
 class Node {
 
-    int first;
-    int second;
-    public Node(int x, int c) {
-        this.first = x;
-        this.second = c;
+    public int first;
+    public int second;
+
+    public Node(int first, int second) {
+        
+        this.first = first;
+        this.second = second;
     }
 }
